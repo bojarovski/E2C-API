f9b24b3407b9429c73f98cea368151e3ca3cc8cf
Mario Bojarovski <bojarovskimario@gmail.com>
2024-10-22 14:32:02 +0200

    Task 3.1.3 - Event and Order routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,15 +1,24 @@
-import express from 'express';
-import { getEvents, getEventById, createEvent, updateEvent,deleteEvent, createEventReview, getTopEvents } from '../controllers/eventController.js';
+import express from "express";
+import {
+  getEvents,
+  getEventById,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+  createEventReview,
+  getTopEvents,
+} from "../controllers/eventController.js";
 
-import {protect, admin} from '../middleware/authMiddleware.js'
+import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.route('/').get(getEvents).post(protect, admin, createEvent);
-router.get('/top', getTopEvents);
-router.route('/:id').get(getEventById).put(protect, admin, updateEvent).delete(protect, admin, deleteEvent);
-router.route('/:id/reviews').post(protect, createEventReview);
-
-
-
+router.route("/").get(getEvents).post(protect, admin, createEvent);
+router.get("/top", getTopEvents);
+router
+  .route("/:id")
+  .get(getEventById)
+  .put(protect, admin, updateEvent)
+  .delete(protect, admin, deleteEvent);
+router.route("/:id/reviews").post(protect, createEventReview);
 
 export default router;
