008be3b4e5fc170a24b1459cec257e5dd60b2685
MarkoMilenovic <markomilenovic7@gmail.com>
2024-10-22 13:41:00 +0200

    # 1.3.3 - WIP :  Add middleware

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 import express from "express";
+import userRoutes from './routes/userRoutes.js';
+import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+const port = process.env.PORT || 5000;
 
 const app = express();
-app.listen(5000, () => {
-  console.log("Server run on port 5000");
+
+connectDB(); // Connect to MongoDB
+app.use(express.json()); // Body parser
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+
+
+app.get('/', (req, res) => {
+    res.send("API is running");
 });
+
+
+app.use('/api/users', userRoutes);
