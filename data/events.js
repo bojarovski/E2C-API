05869f1eb598e2bf48427b589b36b5b1ea348291
VR9444 <rotkiv.r@gmail.com>
2024-11-06 12:39:47 +0100

    package.json fix

---
+++ data/events.js
@@ -0,0 +1,108 @@
+const events = [
+    {
+      
+      "image": "/images/concert.jpg",
+      "name": "Live Concert",
+      "description": "Join us for an unforgettable evening filled with electrifying performances by world-renowned artists and bands. Enjoy an incredible light show, high-quality sound, and an enthusiastic crowd as you immerse yourself in a live music experience like no other. Whether you're a fan of rock, pop, or indie music, this concert will leave you with memories to last a lifetime.",
+      "date": "2025-06-05",
+      "location": "London, UK",
+      "price": 70,
+      "type": "Concert",
+      "countInStock": 250,
+      "rating": 3.5,
+      "numberOfReviews": 342
+    },
+    {
+      
+      "image": "/images/football.jpg",
+      "name": "Football Championship",
+      "description": "Get ready for a heart-pounding final showdown between the best football teams in the league. This championship match will feature fast-paced action, skillful plays, and the excitement of fans from around the world. Whether you're a die-hard football fan or just looking to experience the thrill of a major sporting event, this match will keep you on the edge of your seat.",
+      "date": "2025-07-15",
+      "location": "Paris, France",
+      "price": 90,
+      "type": "Sports",
+      "countInStock": 500,
+      "rating": 4.5,
+      "numberOfReviews": 421
+    },
+    {
+     
+      "image": "/images/malta.jpg",
+      "name": "Malta Island Adventure",
+      "description": "Escape to the beautiful island of Malta, where history and nature come together in perfect harmony. This tour offers an immersive experience that includes visits to ancient landmarks, picturesque beaches, and charming towns. With a local guide, you'll explore the island's rich culture, delicious cuisine, and breathtaking landscapes, making this a trip you won't want to miss.",
+      "date": "2025-08-20",
+      "location": "Valletta, Malta",
+      "price": 150,
+      "type": "Tour",
+      "countInStock": 80,
+      "rating": 2.5,
+      "numberOfReviews": 210
+    },
+    {
+     
+      "image": "/images/maskenball.jpg",
+      "name": "Venetian Masked Ball",
+      "description": "Step into a world of elegance and mystery at the Venetian Masked Ball. This one-of-a-kind event takes place in the heart of Venice, Italy, and features elaborate costumes, traditional Venetian music, and a night of dancing under the stars. Put on your most exquisite mask and join us for an unforgettable evening where fantasy meets reality in a celebration of culture and tradition.",
+      "date": "2025-12-31",
+      "location": "Venice, Italy",
+      "price": 200,
+      "type": "Cultural Event",
+      "countInStock": 150,
+      "rating": 3.5,
+      "numberOfReviews": 189
+    },
+    {
+      
+      "image": "/images/mountaineering.jpg",
+      "name": "Mountaineering Expedition",
+      "description": "For the thrill-seekers and adventure lovers, this mountaineering expedition offers the ultimate outdoor challenge. Set in the majestic Swiss Alps, participants will conquer towering peaks and traverse breathtaking trails while guided by expert climbers. This expedition is designed for experienced climbers seeking a test of endurance and skill, all while enjoying the pristine beauty of the mountains.",
+      "date": "2025-05-10",
+      "location": "Swiss Alps, Switzerland",
+      "price": 300,
+      "type": "Outdoor Adventure",
+      "countInStock": 60,
+      "rating": 4.0,
+      "numberOfReviews": 143
+    },
+    {
+      
+      "image": "/images/native-american.jpg",
+      "name": "Native American Festival",
+      "description": "Celebrate the rich traditions and vibrant history of Native American culture at this unique festival. Enjoy live performances featuring traditional dances, music, and storytelling, as well as opportunities to explore art and craft exhibitions. This cultural event aims to foster greater appreciation and understanding of the indigenous peoples of North America.",
+      "date": "2025-09-12",
+      "location": "Arizona, USA",
+      "price": 50,
+      "type": "Cultural Festival",
+      "countInStock": 500,
+      "rating": 4.5,
+      "numberOfReviews": 98
+    },
+    {
+      
+      "image": "/images/philosophy.jpg",
+      "name": "Philosophy Symposium",
+      "description": "Join a gathering of intellectuals, scholars, and enthusiasts for a thought-provoking symposium on the evolution of modern philosophy. This event will feature keynote speeches by prominent philosophers, panel discussions, and interactive sessions. Whether you're deeply involved in philosophical studies or simply curious, this symposium offers a chance to dive into profound discussions on ethics, existence, and human nature.",
+      "date": "2025-03-22",
+      "location": "Athens, Greece",
+      "price": 120,
+      "type": "Conference",
+      "countInStock": 300,
+      "rating": 4.5,
+      "numberOfReviews": 178
+    },
+    {
+    
+      "image": "/images/pyramids.jpg",
+      "name": "Pyramids of Giza Tour",
+      "description": "Experience the wonders of ancient Egypt with a guided tour of the world-famous Pyramids of Giza. Explore the history and architecture of these monumental structures, which have stood for thousands of years. Learn about the pharaohs, hieroglyphs, and the mysteries that still surround the construction of the pyramids. This tour is perfect for history buffs and adventurers alike.",
+      "date": "2025-04-05",
+      "location": "Giza, Egypt",
+      "price": 180,
+      "type": "Historical Tour",
+      "countInStock": 100,
+      "rating": 5.0,
+      "numberOfReviews": 312
+    }
+  ];
+  
+  export default events;
